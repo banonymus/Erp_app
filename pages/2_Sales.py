3b848f87646b20488bf60fff7d14129121a70585
babis <babis@anonymus.gr>
2026-02-17 17:39:33 +0200

    commit 12 34 16/2/26

--- pages/2_Sales.py
+++ pages/2_Sales.py
@@ -232,7 +232,7 @@ with tab4:
 
 
     if st.button("Download Invoice as PDF"):
-        filename = export_invoice_pdf(orders, items, "invoice.pdf")
+        filename = export_invoice_pdf(orders[0], items, "invoice.pdf")
         with open(filename, "rb") as f:
             st.download_button(
                 "Download PDF",
