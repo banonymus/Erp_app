9b75c7ef6f3091c3c9728cfef4331619170b8e5a
babis <babis@anonymus.gr>
2026-02-26 11:00:39 +0200

    commit 12 34 16/2/26

--- pages/2_Sales.py
+++ pages/2_Sales.py
@@ -311,7 +311,10 @@ with tab4:
                 """, (pdf_order_id,))
         customer_email = cursor.fetchone()[0]
 
+        import os
 
+        st.write("Current working directory:", os.getcwd())
+        st.write("Files in directory:", os.listdir())
 
         if customer_email:
             try:
--- utils/emailer.py
+++ utils/emailer.py
@@ -23,7 +23,7 @@ def send_invoice_email(to_email, subject, body, pdf_path):
 
     BASE_DIR = Path(__file__).resolve().parent
 
-    TEMPLATE_PATH = BASE_DIR.parent / "templates" 
+    TEMPLATE_PATH = BASE_DIR.parent / "templates"
     # Attach PDF
     with open(pdf_path, "rb") as f:
         pdf_data = f.read()
